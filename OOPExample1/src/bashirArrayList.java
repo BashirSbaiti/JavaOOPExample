aa6c711a111b48b297b0d2731fe891ce1e49acb9
BashirSbaiti <bashirsbaiti@gmail.com>
2019-12-27 15:17:20 -0800

    Added override for isEmpty, remove, removeAt to fix iteration

--- OOPExample1/src/bashirArrayList.java
+++ OOPExample1/src/bashirArrayList.java
@@ -1,21 +1,26 @@
 
 public class bashirArrayList<E> extends bashirListFramework<E> implements dynamicSizing<E> {
-	private int capacity;
-	private int size;
+	private int capacity;	// ENCAPSULATION: these variables don't need to be used outside this scope
+	private int size = 0;	// so they are encapsulated in this class
 	
+	// INHERITANCE: this class can use methods from the parent (bashirListFramework)
+	// super() in constructor and extends on first line indicates inheritance
 	public bashirArrayList(int SIZE) {
 		super(SIZE);
 		capacity = SIZE;
 	}
 
-
+	// added element to end of list
+	// implementation of method in interface, must implement this because it is a method that is best
+	// suited for dynamically sized data structures
 	public void pushBack(E element) {
 		data = checkCapacity(data);
 		data[size]=element;
 		size++;
 	}
 
-	public E[] checkCapacity(E[] data) {
+	// doubles capacity if array is full (only to be called before new item is to be added)
+	private E[] checkCapacity(E[] data) {
 		if(size>=capacity) {
 			E[] data2 = (E[])new Object[capacity*2];
 			capacity*=2;
@@ -29,33 +34,65 @@ public class bashirArrayList<E> extends bashirListFramework<E> implements dynami
 		}
 	}
 
+	// inserts element at index
+	// ABSTRACTION: implementation of abstract method, with bounds checking
 	public void insert(E element, int index) {
-		size++;
-		for(int i = size-1; i>index; i--) {
+		data = checkCapacity(data);
+		for(int i = size; i>index; i--) {
 			data[i] = data[i-1];
 		}
 		data[index] = element;
+		size++;
 	}
+	
+	// COMPILE TIME POLYMORPHISM: overload of insert function
+	// assume if index is not specified that a pushBack operation is intended
 	public void insert(E element) {
 		pushBack(element);
 	}
 
+	// RUMTIME POLYMORPHISM: override of remove from parent, 
+	// as parent's implementation doesn't increment size
 	public void remove(E element) {
 		for(int i = find(element); i<size; i++) {
 			data[i]=data[i+1];
 		}
 		size--;
 	}
-
+	
+	// RUMTIME POLYMORPHISM: override of removeAt from parent, for same reason as above
 	public void removeAt(int index) {
 		for(int i = index; i<size; i++) {
 			data[i]=data[i+1];
 		}
 		size--;
 	}
+	
+	// RUMTIME POLYMORPHISM: override of find from parent, uses size rather than SIZE
+	public int find(E element) {
+		for(int i = 0; i<size; i++) {
+			if(data[i]==element) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	// returns if array is full of null values
+	// RUMTIME POLYMORPHISM: override of find from parent, uses size rather than SIZE
+	public boolean isEmpty() {
+		for(int i = 0; i<size; i++) {
+			if(data[i]!=null) {
+				return false;
+			}
+		}
+		return true;
+	}
 
+	// allows the encapsulated variable size to be accessed outside the class but not modified
 	public int size() {
 		return size;
 	}
+	
 
 }
